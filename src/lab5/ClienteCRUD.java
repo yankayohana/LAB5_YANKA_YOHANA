bac9126d1cfb08f1c92e9db117721e466b771f7a
Yanka Yohana Pereira Das Neves 116211079 <yankaypd@lcc3-118.lcc.ufcg.edu.br>
2018-10-30 09:51:39 -0300

    fzr paradinhas

--- src/lab5/ClienteCRUD.java
+++ src/lab5/ClienteCRUD.java
@@ -1,8 +1,37 @@
 package lab5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClienteCRUD {
 	
-	Cliente cliente;
+	private Map<String,Cliente> clientes;
+	private ArrayList<Cliente> clientesOrdenados;
+	
+	public ClienteCRUD() {
+		this.clientes = new HashMap<>();
+		this.clientesOrdenados = new ArrayList<>();
+	}
+	
+	public void creatCliente(String cpf, String nome, String email, String localizacao) {
+		Cliente cliente = new Cliente(cpf, nome, email, localizacao);
+		clientes.put(cpf, cliente);
+		clientesOrdenados.add(cliente);
+		
+	}
+	
+	public String readCliente(String cpf) {
+		return clientes.get(cpf).toString();
+	}
+	
+	public String exibeClientes() {
+		String retorno = "";
+		for (Cliente cliente : clientes.values()) {
+			retorno += cliente.toString() + "|";
+		}
+		return retorno;
+	}
 	
 	
 
